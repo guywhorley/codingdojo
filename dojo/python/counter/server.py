5efa389fe7f3368ece6abfb3d9a745844f5e555e
Guy Whorley <guy.whorley@gmail.com>
2016-05-03 23:26:44 -0700

    finished with counter project.

--- dojo/python/counter/server.py
+++ dojo/python/counter/server.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "ThisIsSecret"
+
+# COUNT BY 1
 @app.route('/')
 def index():
-    return render_template("index.html")
-@app.route('/users', methods=['POST'])
-def create_user():
-    print "Got Post Info"
-    # must store info in the session dictionary object before passing along
-    session['name'] = request.form['name']
-    session['email'] =request.form['email']
-    return redirect('/show') #back to index
-@app.route('/show')
-def show_user():
-    return render_template('user.html')
+    # all values are stored as strings in a dictionary, must cast to number
+    if "count" in session:
+        count = int(session['count'])
+        count += 1
+    else:
+        count = 0 # set init value
+        session['count'] = count
+
+    session['count'] = count # save count for next pass
+    return render_template('index.html', count=session['count'])
+
+# INCREMENT COUNT BY TWO
+@app.route('/double')
+def incrByTwo():
+    count = int(session['count'])
+    count += 2
+    session['count'] = count
+    return render_template('index.html', count=session['count'])
+
+# RESET COUNT
+@app.route('/reset')
+def resetCount():
+    if 'count' in session:
+        del session['count']
+    return redirect('/')
+
 app.run(debug=True)
