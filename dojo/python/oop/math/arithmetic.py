3708e8c9767bfb437591d0a1027931ad4570ff6d
Guy Whorley <guy.whorley@gmail.com>
2016-05-10 16:50:54 -0700

    adding to git

---
+++ dojo/python/oop/math/arithmetic.py
@@ -0,0 +1,25 @@
+# filename: arithmetic.py
+# author: Guy Whorley
+# description: python module with my math functions
+
+def add(x,y):
+    """ Add x+y """
+    return x + y
+
+def multiply(x,y):
+    """ multiply x * y """
+    return x * y
+
+def subtract(x,y):
+    """ subtract x from y """
+    return x - y
+
+def divide(x,y):
+    """ divide x by y """
+    return x / y
+
+def mod(x,y):
+    """ return remainder of modulus x % y """
+    return x % y
+
+    
---
+++ dojo/python/oop/math/test_math.py
@@ -0,0 +1,11 @@
+# filename: test_math.py
+# author: guy Whorley
+# description: my tests for import
+
+import arithmetic as math
+
+
+print math.add(4,3)
+print math.multiply(8,9)
+print math.subtract(10,23)
+print math.mod(10,3)
