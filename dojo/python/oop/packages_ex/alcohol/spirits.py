622f82b0553a0e31c688c0c972b82cb1d6a09a9d
Guy Whorley <guy.whorley@gmail.com>
2016-05-10 17:31:32 -0700

    done with exercise; +alcohol package.

---
+++ dojo/python/oop/packages_ex/alcohol/spirits.py
@@ -0,0 +1,7 @@
+# filename: spirits.py
+# author: guy Whorley
+# description: for the people who prefer harder drinks.
+
+def giveMeDrinks(n):
+    """ Get 'n' number shots of whiskey """
+    return str(n) + " shots of whiskey, coming up!"
---
+++ dojo/python/oop/packages_ex/bartender.py
@@ -0,0 +1,9 @@
+# filename: bartender.py
+# author: guy Whorley
+# description: fun coding for max the developer bartender
+
+from alcohol import *
+
+print spirits.giveMeDrinks(5)
+print wine.giveMeDrinks(5)
+print beer.giveMeDrinks(5)
