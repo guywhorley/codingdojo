9b39ac3d0712fff7e1da45c4f8a9020272c2f505
Guy Whorley <guy.whorley@gmail.com>
2016-06-15 01:42:35 -0700

    done with wall v1

--- dojo/ruby/rails/wall/app/controllers/wall_controller.rb
+++ dojo/ruby/rails/wall/app/controllers/wall_controller.rb
@@ -1,17 +1,65 @@
 class WallController < ApplicationController
 
+  # Wall landing page, show messages and comments.
   def index
-      if !session[:id]
+      if !session.has_key?(:id)
           # need to login or register
           puts "In !session"
-          render 'login'
+          redirect_to action: 'login'
+          return
       end
+      @user = User.find(session[:id])
+      @messages = Msg.all.order(created_at: :desc)
   end
 
-  def create
-    @user = User.create(:name params["last_name"], password: params['password'])
-    render :text => 'creating user'
+  # process login
+  def dologin
+    begin
+      user = User.find_by last_name: params[:last_name]
+      if user.password == params[:password]
+          session[:id] = user.id
+          redirect_to action: 'index'
+          return
+      end
+    rescue # user was not found.
+    end
+    flash[:alert] = "Sorry. Change some things and try again!"
+    redirect_to '/wall/login'
+  end
 
+  # Create the new user
+  def create
+    begin
+        @user = User.create(users_params)
+        if @user.id
+            session[:id] = @user.id
+            redirect_to '/wall'
+            return
+        end
+    rescue
+    end
+    flash[:alert] = "Sorry. Change some things and try again!"
+    redirect_to '/wall/register'
+  end
+
+  # Create message
+  def create_message
+    Msg.create(message_params)
+    redirect_to '/wall'
+  end
+
+  # Show Login Page
+  def login
+  end
+
+  # Logout
+  def logout
+      session[:id] = nil
+      redirect_to action: 'index'
+  end
+
+  # Show Registration Page
+  def register
   end
 
   private
@@ -19,6 +67,10 @@ class WallController < ApplicationController
      # pass to the model/dbase
      def users_params
         params.require(:user).permit(:last_name, :password, :id)
+     end
+
+     def message_params
+        params.permit(:user_id, :message)
      end
 
 
--- dojo/ruby/rails/wall/config/routes.rb
+++ dojo/ruby/rails/wall/config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
 
+  get "wall/register" => "wall#register"
+
+  get "wall/login" => "wall#login"
+
+  post "wall/login" => "wall#dologin"
+
+  post "wall/users/create" => "wall#create"
+
+  get "wall/logout" => "wall#logout"
+
   get "wall" => "wall#index"
+
   post "wall" => "wall#create"
+
+  post "/message" => "wall#create_message"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'wall#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
